c3bc14c361a4a7058c6ad20339f1caba6a89b5fc
Aryan Singh <aryansingh97713@gmail.com>
2025-10-04 19:34:55 +0530

    fix: CI/CD

--- commerce-service/src/main/java/com/singhtwenty2/commerce_service/data/dto/catalogue/ProductImageDTO.java
+++ commerce-service/src/main/java/com/singhtwenty2/commerce_service/data/dto/catalogue/ProductImageDTO.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2025 Aryan Singh
+ * Developer: Aryan Singh (@singhtwenty2)
+ * Portfolio: https://singhtwenty2.pages.dev/
+ * This file is part of SSEW E-commerce Backend System
+ * Licensed under MIT License
+ * For commercial use and inquiries: aryansingh.corp@gmail.com
+ * @author Aryan Singh (@singhtwenty2)
+ * @project SSEW E-commerce Backend System
+ * @since 2025
+ */
 package com.singhtwenty2.commerce_service.data.dto.catalogue;
 
 import jakarta.validation.constraints.*;
