2ceca744ced1eac197774be8df725ff2ed0d3297
Aryan Singh <aryansingh97713@gmail.com>
2025-10-04 22:29:25 +0530

    chore: removed debug statements

--- commerce-service/src/main/java/com/singhtwenty2/commerce_service/config/MailConfig.java
+++ commerce-service/src/main/java/com/singhtwenty2/commerce_service/config/MailConfig.java
@@ -52,7 +52,6 @@ public class MailConfig {
         props.put("mail.smtp.connectiontimeout", "5000");
         props.put("mail.smtp.timeout", "5000");
         props.put("mail.smtp.writetimeout", "5000");
-        props.put("mail.debug", "true");
 
         return mailSender;
     }
